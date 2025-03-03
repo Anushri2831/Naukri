2fba3f92dfb1b742e5acca31839d7369f914473c
Anushri2831 <anushrichinchankar5@gmail.com>
2025-03-03 15:26:17 +0530

    completed the header part

---
+++ my-react-app/src/components/NaukriHeader.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NaukriHeader from "./NaukriHeader";
+
+export default {
+  title: "components/NaukriHeader",
+  component: NaukriHeader,
+};
+
+export const Default = () => <NaukriHeader />;
